9e69c59a660c423e3d2824b5180d77ce154c55cc
cobaltgit <cobaltsecuremail312@proton.me>
2025-05-09 18:26:55 +0100

    Use list comprehension for building rom list

--- main-ui/menus/games/game_select_menu.py
+++ main-ui/menus/games/game_select_menu.py
@@ -23,33 +23,25 @@ class GameSelectMenu(RomsMenuCommon):
         return any(Path(rom_file_path).resolve() == Path(fav.rom_path).resolve() for fav in favorites)
 
     def _build_favorites_dict(self):
-        favorites = self.device.parse_favorites()
-        favorite_paths = []
-        for favorite in favorites:
-            favorite_paths.append(str(Path(favorite.rom_path).resolve()))
-
-        return favorite_paths
-
+        return [str(Path(favorite.rom_path).resolve()) for favorite in self.device.parse_favorites()]
+        
     def _get_rom_list(self) -> list[GridOrListEntry]:
         rom_list = []
         favorites = self._build_favorites_dict()
         start_time = time.time()
 
-        for rom_file_path in self.rom_utils.get_roms(self.game_system):
-            rom_file_name = os.path.basename(rom_file_path)
-            img_path = self._get_image_path(rom_file_path)
-            resolved_folder = str(Path(os.path.dirname(rom_file_path)).resolve())
-            resolved_file_path = resolved_folder+"/"+rom_file_name
-            icon=self.theme.favorite_icon if resolved_file_path in favorites else None
-            rom_list.append(
-                GridOrListEntry(
-                    primary_text=self._remove_extension(rom_file_name),
-                    image_path=img_path,
-                    image_path_selected=img_path,
-                    description=self.game_system, 
-                    icon=icon,
-                    value=rom_file_path)
+        rom_list = [
+            GridOrListEntry(
+                primary_text=self._remove_extension(rom_file_name := os.path.basename(rom_file_path)),
+                image_path=(img_path := self._get_image_path(rom_file_path)),
+                image_path_selected=img_path,
+                description=self.game_system,
+                icon=self.theme.favorite_icon if str(Path(os.path.dirname(rom_file_path)).resolve())+"/"+rom_file_name in favorites else None,
+                value=rom_file_path
             )
+            for rom_file_path in self.rom_utils.get_roms(self.game_system)
+        ]
+
         elapsed = time.time() - start_time
 
         return rom_list
