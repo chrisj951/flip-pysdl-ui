02cbb2d23074c0823ad30596d3b7dbbdd1ce24fb
cobaltgit <cobaltsecuremail312@proton.me>
2025-05-18 17:36:37 +0100

    Fix indentation of bluetooth device option builder

--- main-ui/menus/settings/bluetooth_menu.py
+++ main-ui/menus/settings/bluetooth_menu.py
@@ -71,9 +71,8 @@ class BluetoothMenu:
                     self.should_scan_for_bluetooth = False
                     devices = self.scan_for_devices()
                     selected.index = 0
-
-                for device in devices:
-                    option_list.append(
+                    
+                    option_list.extend(
                         GridOrListEntry(
                                 primary_text=device.name,
                                 value_text=device.address,
@@ -83,6 +82,7 @@ class BluetoothMenu:
                                 icon=None,
                                 value=lambda device=device: self.toggle_pairing_device(device)  
                             )
+                        for device in devices
                     )
 
             list_view = ViewCreator.create_view(
